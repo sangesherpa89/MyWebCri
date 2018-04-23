1523a27224d9fe3823a6efb31550d5465959a87c
sangesherpa89 <37837913+sangesherpa89@users.noreply.github.com>
2018-04-23 15:09:13 -0400

    updated sketch.js
    changed the color

--- WebProJava/Folder/sketch.js
+++ WebProJava/Folder/sketch.js
@@ -7,13 +7,13 @@ function draw() {
   background(255,255,255);
     
   // Light green point light on the right
-  pointLight(1, 3, 1, 500, 0, 200);
+  pointLight(9, 255, 89, 0, 200);
 
   // Red directional light from the left
-  directionalLight(222, 7,59, -1, 0, 0);
+  directionalLight(255, 0, 0, -1, 0, 0);
 
-  // Light blue spotlight from the front
-  pointLight(48, 210, 200, 255, 0, 300);
+  // blue spotlight from the front
+  pointLight(9, 0, 255, 255, 0, 300);
 
   rotateY(map(mouseX, 0, width, 0, PI));
   rotateX(map(mouseY, 0, height, 0, PI));
